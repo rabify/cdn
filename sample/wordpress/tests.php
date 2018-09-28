bf511abc0adcfa2831100fbe1046353c30365943
榊原昌彦 <sakakibara@rdlabo.jp>
2018-09-28 17:14:18 +0900

    chore.

--- sample/wordpress/tests.php
+++ sample/wordpress/tests.php
@@ -16,11 +16,11 @@ $content_width = 600;
 require_once __DIR__ . '/functions.php';
 
 $img_tags = [
-    "<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg\" />",
-    "<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg?v=1\" />",
-    "<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg?d=200\" />",
-    "<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg?v=1&d=200\" />",
-    "<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618-640x360.jpg?v=1\" srcset=\"https://rabify.example.com/wp-content/uploads/2018/06/twitter_top0618.jpg?d=100 100w\" />"
+    "[before]<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg\" />[after]",
+    "[before]<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg?v=1\" />[after]",
+    "[before]<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg?d=200\" />[after]",
+    "[before]<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618.jpg?v=1&d=200\" />[after]",
+    "[before]<img src=\"https://msak-note.com/wp-content/uploads/2018/06/twitter_top0618-640x360.jpg?v=1\" srcset=\"https://rabify.example.com/wp-content/uploads/2018/06/twitter_top0618.jpg?d=100 100w\" />[after]"
 ];
 
 $comp = [];
@@ -28,4 +28,4 @@ foreach($img_tags as $tag) {
     $comp[] = rabify_cdn( $tag );
 }
 
-print_r($comp);
+?>
