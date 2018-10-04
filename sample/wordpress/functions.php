c439c08977ac52f2c6e25285b723f341d33bf5c0
榊原昌彦 <sakakibara@rdlabo.jp>
2018-10-04 22:04:01 +0900

    feature(thambnail): サムネイルのサイズを別途設定できるように変更

--- sample/wordpress/functions.php
+++ sample/wordpress/functions.php
@@ -13,72 +13,78 @@
  *
  * ----------------------------------------------------------------------
  */
-
 define('CDN_URL','https://rabify.example.com');  // 最後のスラッシュはつけないようにしてください
-define('CDN_SIZE', [150, 400, 600, 800, 1000, 1200]);
-define('SRC_SIZE', ''); // https://developer.mozilla.org/ja/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images のsizeを指定
+define('CDN_PATTERN', [150, 200, 400, 600, 800, 1000, 1200]);
+
+/**
+ * WordPress公式Theme Twenty Seventeenのデフォルト値。
+ * 変更する際はこちらを参考。
+ * https://developer.mozilla.org/ja/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images のsizeを指定
+ *
+ * また、https://www.rabify.me/ で計算フォームあります。
+ */
+define('SRC_SIZE', '');
+define('SRC_SIZE_THUMBNAIL', '');
 
 function is_localhost($site_url) {
     if(strpos($site_url,'localhost') !== false) {
         return true;
     }
-
     if(strpos($site_url,'[::1]') !== false) {
         return true;
     }
-
     if(preg_match('/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/', $site_url)){
         return true;
     }
-
     return false;
 }
-
 function rabify_cdn_filter( $the_content ) {
     if(is_localhost(site_url())){
         return $the_content;
     }
-
     $preg_site_url = preg_replace(['/(https?):\/\//', '/\./'], ['$1:\/\/', '\.'], site_url());
     $pattern = "/${preg_site_url}(.*?[\.jpe?g|\.png|\.bmp])/i";
     $replace_content = preg_replace($pattern, CDN_URL."$1", $the_content);
-
     return $replace_content;
 }
-
-function rabify_cdn_srcset( $the_content, $sizes = [] )
+function rabify_cdn_srcset( $the_content, $pattern = [], $sizes = '' )
 {
+    if (count($pattern) === 0) {
+        $pattern = CDN_PATTERN;
+    }
+
+    if (!$sizes) {
+        $sizes = SRC_SIZE;
+    }
+
     $preg_cdn_url = preg_replace('/(https?):\/\//', '$1:\/\/', CDN_URL);
     $preg_cdn_url = preg_replace('/\./', '\.', $preg_cdn_url);
-    $pattern = "/(<img.*?src\=)[\'|\"](${preg_cdn_url}.*?[\.jpe?g|\.png|\.bmp])\??(v\=\w+)?&?(d\=\w+)?[\'|\"](?!.*srcset.*>)/i";
-
+    $reg = "/(<img.*?src\=)[\'|\"](${preg_cdn_url}.*?[\.jpe?g|\.png|\.bmp])\??(v\=\w+)?&?(d\=\w+)?[\'|\"](?!.*srcset.*>)/i";
     $srcset = "srcset=\"";
 
-    if(count($sizes) === 0) {
-        $sizes = CDN_SIZE;
-    }
-
-    foreach($sizes as $size) {
+    foreach($pattern as $size) {
         $srcset .= "$2?$3&d=${size} ${size}w, ";
     }
-    $srcset = rtrim($srcset, ', ') . "\" " . SRC_SIZE;
+    $srcset = rtrim($srcset, ', ') . "\" " . $sizes;
     global $content_width;
-    $replace_content = preg_replace($pattern, "$1\"$2?$3&$4&d=$content_width\" ${srcset} $5", $the_content);
+    if (!$content_width) {
+        $content_width = 600;
+    }
 
+    $replace_content = preg_replace($reg, "$1\"$2?$3&$4&d=$content_width\" ${srcset} $5", $the_content);
     $replace_content = preg_replace('/(<img.*?src\=[\'|\"].*?)(\?d\=\w+)(.*?)d\=\w+(.*?[\'|\"])/', "$1$2$3$4", $replace_content );
     $replace_content = preg_replace('/([\.jpe?g|\.png|\.bmp])\?&/', '$1?', $replace_content );
-
-
     $preg_arrangement = ['/(<img.*?src\=.*?)&&(.*?>)/', '/(<img.*?src\=.*?)[&&?](\'|\")(.*?>)/'];
     $replace_content = preg_replace($preg_arrangement, ['$1&$2', '$1$2$3'], $replace_content );
-
-
     return $replace_content;
 }
-
-function rabify_cdn ( $text, $sizes = [] ) {
+function rabify_cdn ( $text, $pattern = [] ) {
     $text = rabify_cdn_filter( $text );
-    return rabify_cdn_srcset( $text, $sizes );
+    return rabify_cdn_srcset( $text, $pattern );
+}
+
+function rabify_cdn_srcset_thumbnail( $the_content ) {
+    return rabify_cdn_srcset( $the_content, $pattern = [], SRC_SIZE_THUMBNAIL );
 }
 
 /**
@@ -86,26 +92,21 @@ function rabify_cdn ( $text, $sizes = [] ) {
  */
 add_filter( 'the_content', 'rabify_cdn_filter', 1 );
 add_filter( 'the_content', 'rabify_cdn_srcset', 2 );
-
 /**
  * the_excerpt()で表示する画像を、rabify CDNの差し替えます
  */
 add_filter( 'the_excerpt', 'rabify_cdn_filter', 1 );
 add_filter( 'the_excerpt', 'rabify_cdn_srcset', 2 );
-
-/**
- * アイキャッチ画像で表示する画像を、rabify CDNの差し替えます
- */
-add_filter( 'post_thumbnail_html', 'rabify_cdn_filter', 1 );
-add_filter( 'post_thumbnail_html', 'rabify_cdn_srcset', 2 );
-
-
 /**
  * カスタムヘッダーの画像を、rabify CDNの差し替えます
  */
 add_filter( 'custom-header', 'rabify_cdn_filter', 1 );
 add_filter( 'custom-header', 'rabify_cdn_srcset', 2 );
-
+/**
+ * アイキャッチ画像で表示する画像を、rabify CDNの差し替えます
+ */
+add_filter( 'post_thumbnail_html', 'rabify_cdn_filter', 1 );
+add_filter( 'post_thumbnail_html', 'rabify_cdn_srcset_thumbnail', 2 );
 /**
  * デフォルトのsrcsetを有効化する場合、コメントアウトしてください。
  * WordPressのメディアで設定された画像サイズにしたがって、srcsetが設定されます。
